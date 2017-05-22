87096ff88ab35c3a3e883ae55b6cfcf51240e86b
dirtmaxim <dirtmaxim@mail.ru>
2017-05-23 00:26:17 +0300

    Small changes

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    let write_us = document.getElementById("write_us");
-
-    write_us.onclick = function () {
-        write_us.outerHTML = '<span id="mail">support@eachword.ru</span>';
-
-        return false;
-    }
-};
